098ebc10c0cf9c36aac7f4dcac910fd9e215728d
takanashi66 <t.inoue@funaffect.jp>
2018-06-11 00:03:15 +0900

    追加

--- htdocs/common/js/script.js
+++ htdocs/common/js/script.js
@@ -829,6 +829,8 @@ module.exports = containsNode;
 "use strict";
 
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(5);
 
 var _react2 = _interopRequireDefault(_react);
@@ -839,11 +841,63 @@ var _reactDom2 = _interopRequireDefault(_reactDom);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var SClock = function (_React$Component) {
+  _inherits(SClock, _React$Component);
+
+  function SClock(props) {
+    _classCallCheck(this, SClock);
+
+    var _this = _possibleConstructorReturn(this, (SClock.__proto__ || Object.getPrototypeOf(SClock)).call(this, props));
+
+    _this.state = {
+      now: new Date()
+    };
+
+    setInterval(function (e) {
+      _this.setState({ now: new Date() });
+    }, 1000);
+    return _this;
+  }
+
+  _createClass(SClock, [{
+    key: 'render',
+    value: function render() {
+      var now = this.state.now;
+      var hh = this.fmt(now.getHours());
+      var mm = this.fmt(now.getMinutes());
+      var ss = this.fmt(now.getSeconds());
+      return _react2.default.createElement(
+        'div',
+        null,
+        hh,
+        ':',
+        mm,
+        ':',
+        ss
+      );
+    }
+  }, {
+    key: 'fmt',
+    value: function fmt(v) {
+      var s = "00" + v;
+      return s.substr(s.length - 2, 2);
+    }
+  }]);
+
+  return SClock;
+}(_react2.default.Component);
+
 _reactDom2.default.render(_react2.default.createElement(
-  'h1',
+  'div',
   null,
-  'Hello, world!'
-), document.getElementById('root'));
+  _react2.default.createElement(SClock, null)
+), document.getElementById("root"));
 
 /***/ }),
 /* 13 */
--- src/common/js/app.js
+++ src/common/js/app.js
@@ -1,7 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class SClock extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      now:(new Date())
+    }
+    
+    setInterval(e =>{
+      this.setState({now: (new Date())})
+    }, 1000)
+  }
+  
+  render(){
+    const now = this.state.now
+    const hh = this.fmt(now.getHours())
+    const mm = this.fmt(now.getMinutes())
+    const ss = this.fmt(now.getSeconds())
+    return(<div>{hh}:{mm}:{ss}</div>)
+  }
+  
+  fmt(v){
+    const s = "00" + v
+    return s.substr(s.length-2, 2)
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root')
-);
+  <div><SClock /></div>,
+  document.getElementById("root")
+)
